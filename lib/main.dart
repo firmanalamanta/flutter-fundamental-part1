0832f0e96985391207f28c45bbfcee303fdceb82
firmanalamanta <firmanalamanta2001@gmail.com>
2022-08-29 12:57:03 +0700

    flutter1

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Firman Firdaus Alamanta:',
+              'Nama Saya Firman Firdaus Alamanta, sedang mengikuti TSA Mobile Flutter',
             ),
             Text(
               '$_counter',
